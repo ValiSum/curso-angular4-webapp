86430874b19fc4b27ff2004941e10c7d3f31783e
Vali Sum <vali_sum@outlook.com>
2017-07-25 20:55:04 +0200

    Creación de la pagina de Error

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -8,10 +8,10 @@ export class HomeComponent {
 	public titulo: string;
 
 	constructor() {
-		this.titulo = 'Webapp de productos con Angular 4';
+		this.titulo = "Webapp de productos con Angular 4";
 	}
 
 	ngOnInit() {
-		console.log('Se ha cargado el componente home.component.ts');
+		console.log("Se ha cargado el componente home.component.ts");
 	}
 }
